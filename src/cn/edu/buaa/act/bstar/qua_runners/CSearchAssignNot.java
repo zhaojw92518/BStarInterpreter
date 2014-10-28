2c03cd14de95d6a8886fa078c53f1312a4f6148e
zhaojw92518 <516090359@qq.com>
2014-10-28 13:17:36 +0800

    CQuaRunner
    实现Runner

--- src/cn/edu/buaa/act/bstar/qua_runners/CSearchAssignNot.java
+++ src/cn/edu/buaa/act/bstar/qua_runners/CSearchAssignNot.java
@@ -1,5 +1,6 @@
 package cn.edu.buaa.act.bstar.qua_runners;
 
+import cn.edu.buaa.act.bstar.context.CDataEntity;
 import cn.edu.buaa.act.bstar.global.CGlobalDef;
 import cn.edu.buaa.act.bstar.quaternion.CQuaternion;
 import cn.edu.buaa.act.bstar.quaternion.QuaType;
@@ -11,7 +12,18 @@ public class CSearchAssignNot extends CQuaRunner {
 	
 	@Override
 	public int run() {
-		return CGlobalDef.NORMAL;
+		int return_result = CGlobalDef.ERROR;
+		CDataEntity src_data = CDataEntity.create_entity(data_0),
+					search_result = new CDataEntity("int");
+		if(src_data.is_nil){
+			search_result.value_data = 1.0;
+		}
+		else{
+			search_result.value_data = 0.0;
+		}
+		assign_to_local_id(search_result, data_2);
+		
+		return return_result;
 	}
 
 	@Override
